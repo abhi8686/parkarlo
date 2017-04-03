99b2ade9290e92c05332a3b214d6dae08b958b7a
aBHILASH <abhilash@1985.co.in>
2017-04-03 23:52:52 +0530

    user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+def scrape_data json_data
+  json_data.each do |x|
+    lat =x["geometry"]["location"]["lat"]
+    lng = x["geometry"]["location"]["lng"]
+    $data = $data + [[lat, lng]]
+    ParkingSpace.create(latitude: lat, longitude: lng, address: x["name"], user_id: 1, cost: 500)
+    puts "created"
+  end
+end
+require 'open-uri'
+$data = [[17.482899,78.399076]]
+i =0
+while i < 100 do 
+  begin 
+    new_data = $data[i]
+    lat = new_data[0]
+    lng = new_data[1]
+    sleep 5
+    response = open("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat.to_s + "," + lng.to_s + "&radius=10000&type=restaurant&keyword=&key=AIzaSyAJdvRiaxuKEuL8RhE4TE-Hzh5aqEPiz4o").read
+    response = JSON.parse(response)
+    scrape_data(response["results"])
+    i = i+1
+  rescue SocketError => error
+    print "error"
+  end
+end
+
+
+
